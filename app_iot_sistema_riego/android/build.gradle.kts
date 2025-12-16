9211daf4ae5c64af749853a00eb467b77636620b
javierpepito <javiermendezsepulveda19@gmail.com>
2025-12-15 22:59:20 -0300

    ultimo cambio

--- app_iot_sistema_riego/android/build.gradle.kts
+++ app_iot_sistema_riego/android/build.gradle.kts
@@ -1,3 +1,13 @@
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.google.gms:google-services:4.4.0")
+    }
+}
+
 allprojects {
     repositories {
         google()
@@ -21,7 +31,4 @@ subprojects {
 
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
-}
-dependencies {
-    classpath("com.google.gms:google-services:4.4.0")
 }
